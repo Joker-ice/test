b64045519348ad824c41271158a9f5d5e06adc95
Joker-ice <1609456360@qq.com>
2021-11-10 14:54:58 +0800

    代码更新

--- common/test_request.py
+++ common/test_request.py
@@ -1,24 +1,27 @@
 from base64 import encode
 from re import S
 import requests
-from requests.sessions import Session
 from bs4 import BeautifulSoup
-import json
 
-# session = Session()
-# session.post()
-# r = requests.post(url='http://www.baidu.com')
-a = {
-    'url':'https://www.baidu.com',
-    'headers': {"User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64)\
-         AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"},
-    # 'verify':False
-    }
-print(*a)
-res = getattr(requests, 'get')(**a)
-# # res = getattr(requests, 'post')(url='https://www.baidu.com')
-# print(res.text)
-res.encoding='utf8'
-html = res.text
-soup = BeautifulSoup(html, 'html.parser')
-# print(soup.title)
+
+class ApiRequest():
+    def to_request(self, url):
+        data = {
+            'url':url,
+            'headers': {"User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64)\
+                AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"},
+            }
+        res = getattr(requests, 'get')(**data)
+        res.encoding='utf8'
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup.title)
+
+    def get_url(self):
+        for i in range(0,10):
+            url = 'https://movie.douban.com/top250?start=' + str(i*25)
+            self.to_request(url)
+            print(url)
+if __name__ == '__main__':
+    a = ApiRequest()
+    a.get_url()
